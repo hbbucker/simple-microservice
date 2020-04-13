201791b97f394bb19aa06a31b732db65d49a087e
Hugo Bastos Bucker <hugo.bucker@qliktec.com.br>
2020-04-13 17:46:40 -0400

    commit inicial do projeto

---
+++ src/test/java/br/com/bucker/resource/ClienteResourceTest.java
@@ -0,0 +1,22 @@
+package br.com.bucker.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ClienteResourceTest {
+
+    @Test
+    public void buscarClientePeloIdTeste() {
+
+        given()
+          .when().get("/cliente/1")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
---
+++ src/test/java/br/com/bucker/resource/NativeClienteIT.java
@@ -0,0 +1,9 @@
+package br.com.bucker.resource;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeClienteIT extends ClienteResourceTest {
+
+    // Execute the same tests but in native mode.
+}
